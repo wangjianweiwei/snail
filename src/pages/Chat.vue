a4bc4c0560fb205b41d6b48500e14ec713ee5cc1
WangJianWei <594504110@qq.com>
2024-09-11 00:23:38 +0800

    update: chat布局

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -13,127 +13,183 @@ const getAvatar = () => {
 </script>
 
 <template>
-  <v-layout class="rounded-lg mx-6">
-    <v-navigation-drawer width="380">
-      <v-sheet>
-        <div class="d-flex gap-2 pa-6">
-          <div>
-            <v-avatar size="60">
-              <v-img
-                alt="John"
-                src="https://adminpro-vuetify-main.netlify.app/assets/user-1-CznVQ9Sv.jpg"
-              ></v-img>
-            </v-avatar>
-          </div>
-          <div>
-            <h5 class="text-subtitle-1 font-weight-medium">Mathew Anderson</h5>
-            <h6 class="text-subtitle-2">Designer</h6>
-          </div>
-        </div>
-      </v-sheet>
-      <v-sheet>
-        <div class="d-flex gap-2 align-center pa-6">
-          <v-text-field
-            :loading="loading"
-            append-inner-icon="mdi-magnify"
-            density="compact"
-            variant="outlined"
-            color="blue"
-            placeholder="Search Contact"
-            @click:append-inner="onClick"
-          ></v-text-field>
-        </div>
+  <div style="height: 100%" class="pa-10">
+    <v-row justify="center" no-gutters style="height: 100%">
+      <v-col lg="12" xl="8">
+        <v-card border rounded="lg" style="height: 100%" class="pa-5 mx-10">
+          <v-row style="height: 8%" class="border">
+            <v-col cols="3">
+              <v-sheet>
+                头像
+              </v-sheet>
+            </v-col>
+            <v-col cols="9">
+              <v-sheet>
+                <p>对话工具栏</p>
+              </v-sheet>
+            </v-col>
+          </v-row>
+          <v-row style="height: 92%" class="border">
+            <v-col cols="3" style="height: 100%;overflow-y: scroll">
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+              <p>好友列表</p>
+            </v-col>
+            <v-col cols="9" style="height: 100%">
+              <v-row style="height: 90%;overflow-y: scroll" class="border">
+                <v-col class="pa-3">
+                  <v-sheet>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                    <p>聊天内容</p>
+                  </v-sheet>
+                </v-col>
+              </v-row>
+              <v-row style="height: 10%" class="border">
+                <v-col>
+                  <v-sheet>
+                    <v-textarea no-resize center-affix rows="3">
+                      <template #append-inner>
+                        <v-icon icon="$vuetify"></v-icon>
+                        <v-btn>发送</v-btn>
+                      </template>
+                    </v-textarea>
+                  </v-sheet>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
 
-      </v-sheet>
-      <v-sheet>
-        <v-list class="pa-2" activatable nav lines="two">
-          <div v-for="n in 5">
-            <v-list-item
-              style="margin-bottom: 0"
-              color="blue"
-              density="comfortable"
-              :prepend-avatar="getAvatar()"
-              subtitle="john@google.com"
-              title="John Leider"
-            >
-              <template v-slot:append>
-                <span class="v-list-item-subtitle">14 minutes</span>
-              </template>
-            </v-list-item>
-            <v-divider></v-divider>
-          </div>
 
-        </v-list>
-      </v-sheet>
-    </v-navigation-drawer>
+        </v-card>
+      </v-col>
+    </v-row>
 
-    <v-app-bar height="80">
-      <template #title>
-        <v-list-item
-          color="blue"
-          density="comfortable"
-          :prepend-avatar="getAvatar()"
-          subtitle="online"
-          title="John Leider"
-        >
-          <template v-slot:append>
-            <v-icon class="ga-3" icon="mdi-phone"></v-icon>
-            <v-icon class="ga-3" icon="mdi-video"></v-icon>
-          </template>
-        </v-list-item>
-      </template>
-
-    </v-app-bar>
-
-    <v-main>
-     <div style="height: 40%">
-        <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-      <p>Main Content</p>
-     </div>
-    </v-main>
-  </v-layout>
+  </div>
 </template>
 
 <style scoped>
+.v-row {
+  flex-wrap: nowrap;
+}
+
+.v-field__append-inner {
+  align-items: center;
+}
 </style>
